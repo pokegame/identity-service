ca835d0647ced9a500c61356a38c4c0a7f09a6d1
Federkun <federkun@gmail.com>
2017-09-10 20:06:32 +0200

    Add FrameworkExtraBundle to the deps

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
     'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
     'Doctrine\Bundle\DoctrineBundle\DoctrineBundle' => ['all' => true],
+    'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle' => ['all' => true],
 ];
